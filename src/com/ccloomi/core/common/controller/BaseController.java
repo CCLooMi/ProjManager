6edce9fd68e2742da33270f67137acaabcbf24be
CCLooMi <1004178387@qq.com>
2015-06-23 16:46:48 +0800

    add baseController
    add baseController

---
+++ src/com/ccloomi/core/common/controller/BaseController.java
@@ -0,0 +1,10 @@
+package com.ccloomi.core.common.controller;
+/**
+ * 类名：BaseController
+ * 描述：基础抽象controller
+ * 作者： Chenxj
+ * 日期：2015年6月23日 - 下午4:34:11
+ */
+public abstract class BaseController {
+	
+}
