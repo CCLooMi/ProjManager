ecb18da132bdfcca7cb47b6c0f5b18230cbcb711
CCLooMi <1004178387@qq.com>
2015-07-04 09:26:52 +0800

    解决不能保存数据问题
    解决不能保存数据问题

--- src/com/ccloomi/core/common/service/imp/BaseServiceImp.java
+++ src/com/ccloomi/core/common/service/imp/BaseServiceImp.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.Map;
 
+import javax.transaction.Transactional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,7 @@ import com.ccloomi.core.common.service.BaseService;
  * 日期：2015年6月23日 - 下午4:20:02
  */
 @Service("baseService")
+@Transactional
 public class BaseServiceImp<T> implements BaseService<T>{
 	protected final Logger log=LoggerFactory.getLogger(this.getClass());
 	@Autowired
