cb898de144fa9b53b967667ca6b62a0ca2ec2ab9
chenxianjun <1004178387@qq.com>
2015-07-13 23:01:18 +0800

    sender mail
    sender mail

---
+++ src/com/ccloomi/web/test/MailSenderTest.java
@@ -0,0 +1,24 @@
+package com.ccloomi.web.test;
+
+import com.ccloomi.core.component.email.MailSender;
+import com.ccloomi.core.component.email.bean.Mail;
+import com.ccloomi.test.BaseSpringTest;
+
+/**© 2015-2015 CCLooMi.Inc Copyright
+ * 类    名：MailSenderTest
+ * 类 描 述：
+ * 作    者：Chenxj
+ * 邮    箱：chenios@foxmail.com
+ * 日    期：2015年7月13日-下午10:46:47
+ */
+public class MailSenderTest extends BaseSpringTest{
+	public static void main(String[] args) {
+		MailSender ms=ctx.getBean("mailSender", MailSender.class);
+		Mail mail=new Mail();
+		mail.setSubject("Test mail.");
+		mail.setTo("chenxianjun@idccapp.com");
+		mail.setHtmlBody("Hello this is a test mail.");
+		mail.setHtml(true);
+		ms.sendMail(mail);
+	}
+}
