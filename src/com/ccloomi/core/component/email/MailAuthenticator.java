bfefe6247a5af38f3e5ba8aff0d8e9f60d454d95
CCLooMi <1004178387@qq.com>
2015-07-13 18:23:10 +0800

    add mail
    add mail

---
+++ src/com/ccloomi/core/component/email/MailAuthenticator.java
@@ -0,0 +1,12 @@
+package com.ccloomi.core.component.email;
+
+import javax.mail.Authenticator;
+/**
+ * 类名：MailAuthenticator
+ * 描述：邮箱权限验证
+ * 作者： Chenxj
+ * 日期：2015年7月13日 - 下午6:20:42
+ */
+public class MailAuthenticator extends Authenticator{
+	
+}
---
+++ src/com/ccloomi/core/component/email/MailSenderFactory.java
@@ -0,0 +1,10 @@
+package com.ccloomi.core.component.email;
+/**
+ * 类名：MailSenderFactory
+ * 描述：MailSender工厂类
+ * 作者： Chenxj
+ * 日期：2015年7月13日 - 下午6:22:26
+ */
+public class MailSenderFactory {
+
+}
