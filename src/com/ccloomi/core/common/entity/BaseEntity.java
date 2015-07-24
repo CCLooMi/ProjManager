eb420537aa18414e2673e6d4febd6a1166393604
CCLooMi <1004178387@qq.com>
2015-07-24 14:59:07 +0800

    优化
    优化

--- src/com/ccloomi/core/common/entity/BaseEntity.java
+++ src/com/ccloomi/core/common/entity/BaseEntity.java
@@ -1,13 +1,16 @@
 package com.ccloomi.core.common.entity;
 
-import javax.persistence.MappedSuperclass;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import javax.persistence.MappedSuperclass;
 
 import com.ccloomi.core.common.bean.BaseBean;
 import com.ccloomi.core.common.exception.PropertyNotExistInEntityException;
-import com.ccloomi.core.util.StringUtil;
 
 /**© 2015-2015 CCLooMi.Inc Copyright
  * 类    名：BaseEntity
@@ -17,25 +20,75 @@ import com.ccloomi.core.util.StringUtil;
  * 日    期：2015年7月15日-下午8:24:32
  */
 public abstract class BaseEntity extends BaseBean{
-	private static final long serialVersionUID = 7576292943312235832L;
-	protected final Logger log=LoggerFactory.getLogger(getClass());
-	public String getProperty(String name){
-		return getProperty(getClass(), name);
+	private static final long serialVersionUID = 4195520472702749062L;
+	private List<String>propertiesA=new ArrayList<String>();
+	private List<String>propertiesV=new ArrayList<String>();
+	private Map<String, String>propertiesMap=new HashMap<String, String>();
+	public BaseEntity() {
+		findAllProperties(getClass());
+		Collections.reverse(propertiesA);
 	}
-	private String getProperty(Class<?>c,String name){
-		String pname=null;
-		try {
-			pname=c.getDeclaredField(name).getName();
-		} catch (NoSuchFieldException e) {
-			if(superClassHasMappedSuperclassAnnotation(c)){
-				pname=getProperty(c.getSuperclass(), name);
-			}else{
-				log.error(StringUtil.format("在?中找不到属性：?", getClass().getName(),name), new PropertyNotExistInEntityException());
-			}
-		} catch (SecurityException e) {
-			e.printStackTrace();
+	/**
+	 * 方法描述：通过名称查找
+	 * 作者：Chenxj
+	 * 日期：2015年7月24日 - 下午2:58:00
+	 * @param name
+	 * @return
+	 */
+	public String getProperty(String name){
+//		return getProperty(getClass(), name);
+		if(propertiesMap.containsKey(name)){
+			return propertiesMap.get(name);
+		}else{
+			throw new PropertyNotExistInEntityException("在"+getClass().getName()+"找不到属性："+name);
 		}
-		return pname;
+	}
+	/**
+	 * 方法描述：从最顶层开始查找
+	 * 作者：Chenxj
+	 * 日期：2015年7月24日 - 下午2:57:03
+	 * @param index
+	 * @return
+	 */
+	public String getPropertyA(int index){
+		return propertiesA.get(index);
+	}
+	/**
+	 * 方法描述：从本实体开始向上查找
+	 * 作者：Chenxj
+	 * 日期：2015年7月24日 - 下午2:57:08
+	 * @param index
+	 * @return
+	 */
+	public String getPropertyV(int index){
+		return propertiesV.get(index);
+	}
+//	private String getProperty(Class<?>c,String name){
+//		String pname=null;
+//		try {
+//			pname=c.getDeclaredField(name).getName();
+//		} catch (NoSuchFieldException e) {
+//			if(superClassHasMappedSuperclassAnnotation(c)){
+//				pname=getProperty(c.getSuperclass(), name);
+//			}else{
+//				throw new PropertyNotExistInEntityException("在"+getClass().getName()+"找不到属性："+name);
+//			}
+//		} catch (SecurityException e) {
+//			e.printStackTrace();
+//		}
+//		return pname;
+//	}
+	private void findAllProperties(Class<?>c){
+		Field[]fields=c.getDeclaredFields();
+		int l=fields.length;
+		for(Field f:fields){
+			propertiesV.add(f.getName());
+			propertiesA.add(fields[--l].getName());
+			propertiesMap.put(f.getName(),f.getName());
+		}
+		if(superClassHasMappedSuperclassAnnotation(c)){
+			findAllProperties(c.getSuperclass());
+		}
 	}
 	private boolean superClassHasMappedSuperclassAnnotation(Class<?>c){
 		return c.getSuperclass().getDeclaredAnnotation(MappedSuperclass.class)==null?false:true;
