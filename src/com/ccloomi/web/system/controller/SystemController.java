0d0ccf03b86b02f58cc1f0ddd5194ff168872252
chenxianjun <1004178387@qq.com>
2015-07-30 23:34:33 +0800

    去掉多余代码
    去掉多余代码

--- src/com/ccloomi/web/system/controller/SystemController.java
+++ src/com/ccloomi/web/system/controller/SystemController.java
@@ -1,19 +1,16 @@
 package com.ccloomi.web.system.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
-@SessionAttributes({"userid"})
 public class SystemController {
 	@RequestMapping("/login")
 	public String login(){
 		return "login";
 	}
 	@RequestMapping("/home")
-	public String home(@ModelAttribute("userid") String userid){
+	public String home(){
 		return "home";
 	}
 }
