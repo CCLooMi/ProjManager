397d6bf7ecb8853de649c6908b596ec93bb699a1
chenxianjun <1004178387@qq.com>
2015-07-30 22:40:12 +0800

    login
    login

--- src/com/ccloomi/web/system/controller/SystemController.java
+++ src/com/ccloomi/web/system/controller/SystemController.java
@@ -1,16 +1,19 @@
 package com.ccloomi.web.system.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes({"userid"})
 public class SystemController {
 	@RequestMapping("/login")
 	public String login(){
 		return "login";
 	}
 	@RequestMapping("/home")
-	public String home(){
+	public String home(@ModelAttribute("userid") String userid){
 		return "home";
 	}
 }
--- src/com/ccloomi/web/system/controller/UserController.java
+++ src/com/ccloomi/web/system/controller/UserController.java
@@ -3,6 +3,8 @@ package com.ccloomi.web.system.controller;
 import java.io.Serializable;
 import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,7 +41,7 @@ public class UserController extends BaseController{
 
 	@RequestMapping("/checkUser")
 	@ResponseBody
-	public Message checkUser(@RequestBody Map<String, String>map){
+	public Message checkUser(@RequestBody Map<String, String>map,HttpSession session){
 		Message ms=new Message();
 		String username=map.get("username");
 		String password=map.get("password");
@@ -47,6 +49,7 @@ public class UserController extends BaseController{
 		if(id!=null){
 			ms.setCode("0");
 			ms.setInfo((String)id);
+			session.setAttribute("userid",id);
 		}else{
 			ms.setCode("1");
 			ms.setInfo("用户名不存在或密码错误！");
