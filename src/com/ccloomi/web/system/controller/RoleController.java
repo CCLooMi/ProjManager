a4e2d16338ddce58fe3699a12046faa2bec731df
chenxianjun <1004178387@qq.com>
2015-08-12 22:39:05 +0800

    bug fix
    bug fix

--- src/com/ccloomi/web/system/controller/RoleController.java
+++ src/com/ccloomi/web/system/controller/RoleController.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -26,7 +27,7 @@ public class RoleController extends BaseController{
 	private RoleService roleService;
 	@RequestMapping("/add")
 	@ResponseBody
-	public Message addRole(RoleEntity role){
+	public Message addRole(@RequestBody RoleEntity role){
 		Message m=new Message();
 		Serializable roleid=roleService.save(role);
 		if(roleid!=null){
